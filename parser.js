3f00ef3f9b0f0eabdeb92f509775ba13d7dfd4c7
Amish Shah <amishshah.2k@gmail.com>
2015-11-22 16:38:09 +0000

    Added function to see if a message is a valid command type
    Works on multiple levels

--- parser.js
+++ parser.js
@@ -1,7 +1,21 @@
-exports.command = function(prefix, message, options){
+function DefaultOptions(defaults, options) {
+	for (var key in defaults) {
+		if (!options.hasOwnProperty(key)){
+			options[key] = defaults[key];
+		}
+	}
+	
+	return options;
+}
 
-	options.sensitive = false;
-	options.allowMention = true;
+var defaults = {
+	sensitive : false,
+	allowMention : false
+}
+
+exports.command = function (prefix, message, options) {
+
+	options = DefaultOptions(defaults, options);
 
 	/*
 		>>help command subcommand arg1=bop arg2="bop bop" -fl --verbose
@@ -19,6 +33,24 @@ exports.command = function(prefix, message, options){
 		}	
 	*/
 	
-	
-	
+	function isValidCommandType(){
+		if(options.sensitive){
+			// don't trim, keep case sensitivity
+			return message.content.indexOf(prefix) === 0;
+		}else if(options.allowMention){
+			// see if the user is mentioned
+			if(message.isMentioned(options.allowMention)){
+				message.content = message.content.replace(options.allowMention.mention(), "");
+				return true;
+			}else{
+				return false;
+			}
+				
+		}else{
+			// trim, don't care about case
+			var insen = message.content.trim().toUpperCase();
+			return message.content.indexOf(prefix.toUpperCase()) === 0;
+		}
+	}
+
 }
